2cc6fd555f5684b161455d5754c1397baf50c4d4
Diego Pertierra <dpertierra@hotmail.com>
2024-07-05 01:20:33 +0200

    adding plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,68 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+        delete_to_trash = false,
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+  {
+    'pocco81/auto-save.nvim',
+    enabled = true,
+    execution_message = {
+      message = function() -- message to print on save
+        return ('AutoSave: saved at ' .. vim.fn.strftime '%H:%M:%S')
+      end,
+      dim = 0.18, -- dim the color of `message`
+      cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
+  },
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup()
+    end,
+  },
+  {
+    'f-person/git-blame.nvim',
+    config = function()
+      require('gitblame').setup {
+        --Note how the `gitblame_` prefix is omitted in `setup`
+        enabled = true,
+      }
+    end,
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup {}
+    end,
+  },
+  {
+    'gelguy/wilder.nvim',
+    keys = { '/', '?', ':' },
+    build = ':UpdateRemotePlugins',
+  },
+}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,14 +12,179 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '<C-b>', ':Neotree reveal<CR>', { desc = 'NeoTree reveal', remap = true } },
   },
   opts = {
+    window = {
+      mappings = {
+        -- ['\\'] = 'close_window',
+        ['<space>'] = {
+          'toggle_node',
+          nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+        },
+        ['<2-LeftMouse>'] = 'open',
+        ['<cr>'] = 'open',
+        ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
+        ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+        -- Read `# Preview Mode` for more information
+        ['l'] = 'focus_preview',
+        ['S'] = 'open_split',
+        ['s'] = 'open_vsplit',
+        -- ["S"] = "split_with_window_picker",
+        -- ["s"] = "vsplit_with_window_picker",
+        ['t'] = 'open_tabnew',
+        -- ["<cr>"] = "open_drop",
+        -- ["t"] = "open_tab_drop",
+        ['w'] = 'open_with_window_picker',
+        --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+        ['C'] = 'close_node',
+        -- ['C'] = 'close_all_subnodes',
+        ['z'] = 'close_all_nodes',
+        --["Z"] = "expand_all_nodes",
+        ['a'] = {
+          'add',
+          -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+          -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+          config = {
+            show_path = 'none', -- "none", "relative", "absolute"
+          },
+        },
+        ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+        ['d'] = 'delete',
+        ['r'] = 'rename',
+        ['y'] = 'copy_to_clipboard',
+        ['x'] = 'cut_to_clipboard',
+        ['p'] = 'paste_from_clipboard',
+        ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
+        -- ["c"] = {
+        --  "copy",
+        --  config = {
+        --    show_path = "none" -- "none", "relative", "absolute"
+        --  }
+        --}
+        ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
+        ['q'] = 'close_window',
+        ['R'] = 'refresh',
+        ['?'] = 'show_help',
+        ['<'] = 'prev_source',
+        ['>'] = 'next_source',
+        ['i'] = 'show_file_details',
+        ['G'] = 'git_status',
+      },
+    },
     filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        -- hide_dotfiles = true,
+        hide_gitignored = true,
+        -- hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          'node_modules',
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          -- '.gitignored',
+        },
+        always_show_by_pattern = { -- uses glob style patterns
+          --".env*",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          --".DS_Store",
+          --"thumbs.db"
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<bs>'] = 'navigate_up',
+          ['.'] = 'set_root',
+          ['H'] = 'toggle_hidden',
+          ['/'] = 'fuzzy_finder',
+          ['D'] = 'fuzzy_finder_directory',
+          ['#'] = 'fuzzy_sorter', -- fuzzy sorting using the fzy algorithm
+          -- ["D"] = "fuzzy_sorter_directory",
+          ['f'] = 'filter_on_submit',
+          ['<c-x>'] = 'clear_filter',
+          ['[g'] = 'prev_git_modified',
+          [']g'] = 'next_git_modified',
+          ['o'] = { 'show_help', nowait = false, config = { title = 'Order by', prefix_key = 'o' } },
+          ['oc'] = { 'order_by_created', nowait = false },
+          ['od'] = { 'order_by_diagnostics', nowait = false },
+          ['og'] = { 'order_by_git_status', nowait = false },
+          ['om'] = { 'order_by_modified', nowait = false },
+          ['on'] = { 'order_by_name', nowait = false },
+          ['os'] = { 'order_by_size', nowait = false },
+          ['ot'] = { 'order_by_type', nowait = false },
+          ['G'] = { 'git_status', nowait = false },
+          -- ['<key>'] = function(state) ... end,
+        },
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          ['<down>'] = 'move_cursor_down',
+          ['<C-n>'] = 'move_cursor_down',
+          ['<up>'] = 'move_cursor_up',
+          ['<C-p>'] = 'move_cursor_up',
+          -- ['<key>'] = function(state, scroll_padding) ... end,
         },
       },
     },
+    icon = {
+      folder_closed = '',
+      folder_open = '',
+      folder_empty = '󰜌',
+      -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+      -- then these will never be used.
+      default = '*',
+      highlight = 'NeoTreeFileIcon',
+    },
+    modified = {
+      symbol = '[+]',
+      highlight = 'NeoTreeModified',
+    },
+    name = {
+      trailing_slash = false,
+      use_git_status_colors = true,
+      highlight = 'NeoTreeFileName',
+    },
+    git_status = {
+      symbols = {
+        -- Change type
+        added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = '✖', -- this can only be used in the git_status source
+        renamed = '󰁕', -- this can only be used in the git_status source
+        -- Status type
+        untracked = '',
+        ignored = '',
+        unstaged = '󰄱',
+        staged = '',
+        conflict = '',
+      },
+      window = {
+        position = 'float',
+        mappings = {
+          ['gA'] = 'git_add_all',
+          ['gu'] = 'git_unstage_file',
+          ['ga'] = 'git_add_file',
+          ['gr'] = 'git_revert_file',
+          ['gc'] = 'git_commit',
+          ['gp'] = 'git_push',
+          ['gg'] = 'git_commit_and_push',
+          ['o'] = { 'show_help', nowait = false, config = { title = 'Order by', prefix_key = 'o' } },
+          ['oc'] = { 'order_by_created', nowait = false },
+          ['od'] = { 'order_by_diagnostics', nowait = false },
+          ['om'] = { 'order_by_modified', nowait = false },
+          ['on'] = { 'order_by_name', nowait = false },
+          ['os'] = { 'order_by_size', nowait = false },
+          ['ot'] = { 'order_by_type', nowait = false },
+        },
+      },
+    },
+    close_if_last_window = false,
+    enable_git_status = true,
   },
 }
