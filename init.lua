561e4d26b9f622589393996cc339a88e7555f5ee
Diego Pertierra <dpertierra@hotmail.com>
2024-07-10 17:06:46 +0200

    plugins and configs

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -857,7 +857,6 @@ require('lazy').setup({
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
-
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -62,7 +62,110 @@ return {
   },
   {
     'gelguy/wilder.nvim',
-    keys = { '/', '?', ':' },
     build = ':UpdateRemotePlugins',
+    keys = { '/', '?', ':' },
+    config = function()
+      local wilder = require 'wilder'
+      wilder.setup {
+        modes = { ':', '/', '?' },
+        next_key = '<Down>',
+        previous_key = '<Up>',
+        accept_key = '<CR>',
+        reject_key = '<Esc>',
+      }
+      wilder.set_option('pipeline', {
+        wilder.branch(
+          wilder.python_file_finder_pipeline {
+            -- to use ripgrep : {'rg', '--files'}
+            -- to use fd      : {'fd', '-tf'}
+            file_command = { 'find', '.', '-type', 'f', '-printf', '%P\n' },
+            -- to use fd      : {'fd', '-td'}
+            dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
+            -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
+            -- found at https://github.com/nixprime/cpsm
+            filters = { 'fuzzy_filter', 'difflib_sorter' },
+          },
+          wilder.cmdline_pipeline(),
+          wilder.python_search_pipeline()
+        ),
+      })
+      wilder.set_option(
+        'renderer',
+        wilder.popupmenu_renderer {
+          -- highlighter applies highlighting to the candidates
+          highlighter = wilder.basic_highlighter(),
+          left = { ' ', wilder.popupmenu_devicons() },
+          right = { ' ', wilder.popupmenu_scrollbar() },
+          -- wilder.popupmenu_border_theme {
+          --   highlights = {
+          --     border = 'Normal', -- highlight to use for the border
+          --   },
+          --   -- 'single', 'double', 'rounded' or 'solid'
+          --   -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+          --   border = 'rounded',
+          -- },
+        }
+      )
+    end,
+  },
+  { 'stevearc/dressing.nvim' },
+  {
+    'gbprod/yanky.nvim',
+    cmd = { 'YankyRingHistory' },
+    config = function()
+      require 'config.yanky'
+    end,
+  },
+  { 'michaeljsmith/vim-indent-object', event = 'VeryLazy' },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+  },
+  {
+    'kylechui/nvim-surround',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup()
+    end,
+  },
+  {
+    'max397574/colortils.nvim',
+    cmd = 'Colortils',
+    config = function()
+      require('colortils').setup()
+    end,
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+    },
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    -- config = function()
+    --   local rainbow_delimiters = require 'rainbow-delimiters'
+    --   rainbow_delimiters.setup {
+    --     strategy = {
+    --       [''] = rainbow_delimiters.strategy['global'],
+    --       commonlisp = rainbow_delimiters.strategy['local'],
+    --     },
+    --     query = {
+    --       [''] = 'rainbow-delimiters',
+    --       latex = 'rainbow-blocks',
+    --     },
+    --     highlight = {
+    --       'RainbowDelimiterRed',
+    --       'RainbowDelimiterYellow',
+    --       'RainbowDelimiterBlue',
+    --       'RainbowDelimiterOrange',
+    --       'RainbowDelimiterGreen',
+    --       'RainbowDelimiterViolet',
+    --       'RainbowDelimiterCyan',
+    --     },
+    --     blacklist = { 'c', 'cpp' },
+    --   }
+    -- end,
   },
 }
